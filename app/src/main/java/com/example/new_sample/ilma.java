48ef1c36ce6eb091f588dc9ea602b2301a738215
ilma105 <ilmahossain1707105@gmail.com>
2021-11-21 22:13:53 +0600

    add a new java class

---
+++ app/src/main/java/com/example/new_sample/ilma.java
@@ -0,0 +1,18 @@
+package com.example.new_sample;
+
+public class ilma {
+    int x;
+
+    public ilma(int x) {
+
+        this.x = x;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+}
